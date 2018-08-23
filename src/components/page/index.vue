0c7485a75d9c548ad4c50a1adf0121cfae351599
zyh <291821728@qq.com>
2018-08-23 09:40:12 +0800

    全部

---
+++ src/components/page/index.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="header">
+		<div class="header-left">
+			<span class="iconfont">&#xe624;</span>
+		</div>
+		<div class="header-input">
+			<input type="text" class="ipt" v-model="val" placeholder="请输入景点...">
+		</div>
+		<div class="header-right">
+			城市
+			<span class="iconfont">&#xe64a;</span>
+		</div>
+	</div>
+</template>
+<script>
+	export default{
+		data(){
+			return {
+				val:""
+			}	
+		},
+		watch:{
+			val(value){
+				console.log(value)
+			}
+		}
+	}
+</script>
+<style scoped lang="scss">
+	@import '../css/all.styl';
+	$color:red;
+    .ipt::-webkit-input-placeholder{
+        color:$color;
+    }
+	.header{
+		line-height: .86rem;
+		background:$bg;
+		display: flex;
+		color: #fff;
+		.header-left{
+			width: .64rem;
+			float: left;
+			margin-left: .2rem;
+			span{
+				font-size: .3rem;
+			}
+		}
+		.header-input{
+			flex:1;
+			.ipt{
+				width: 94%;
+				border:0 none;
+				outline:none;
+				background-color: #fff;
+				border-radius: .1rem;
+				height: .64rem;
+				margin-left: .2rem;
+				padding-left: .2rem;
+			}
+		}
+		.header-right{
+			width: 1.24rem;
+			float: right;
+			text-align:center;
+			font-size: .3rem;
+		}
+	}
+</style> 
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'lib-flexible/flexible.js'
+import './assets/iconfont.css'
+
+
+
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
+// console.log(VueAwesomeSwiper)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
